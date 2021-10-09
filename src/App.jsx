8b1a9fb423a3820080b461042e4e5cdf8270ed4a
ismailinayat <ismailinayat@gmail.com>
2021-10-09 14:12:41 +0500

    Ai Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/navbar/NavbarMUI';
 import Header from './components/header/Header';
 import Team from './components/Our team/Team';
 import Contact from './components/Contact Us/Contact';
+import AI from './components/AI/AI';
 
 
 //const colorDark = "#1f2937"
@@ -13,6 +14,7 @@ function App() {
       <div>
         <Navbar/>
         <Header/>
+        <AI></AI>
         <Team/>
         <Contact/>
       </div>
---
+++ src/components/AI/AI.jsx
@@ -0,0 +1,49 @@
+import './ai.scss';
+
+
+//const colorDark = "#1f2937"
+//const colorPrimary = "#eabd23"
+//const colorLight = "rgb(55,65,81)"
+// const colorText = 
+
+const AI = () => {
+    return (
+        <div className="ai_section">
+
+            <div className="container">
+
+                <div className="container__heading">
+
+                    <div className="container__heading--primary">
+                        <h2>
+                            We Eliminate Bottlenecks using <span>Artificial Intelligence</span>
+                        </h2>
+                    </div>
+
+                    <div className="container__heading--secondary">
+                        <p className= "paragraph paragraph__1">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nisi veritatis voluptates animi, totam <ratione className=""></ratione> Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        </p>
+
+                        <p className= "paragraph paragraph__2">
+                            Qui nisi veritatis voluptates animi, totam ratione fugit odit quo quia ab repellendus est praesentium non facilis facere atque hic neque a. Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        </p>
+
+                        <p className= "paragraph paragraph__3">eritatis voluptates animi, totam ratione fugit odit quo quia ab repellendus est praesentium non facilis facere atque hic neque a. Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nisi v
+                        </p>
+                    </div>
+
+                </div>
+
+                <div className="container__image">
+                    
+                </div>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default AI
